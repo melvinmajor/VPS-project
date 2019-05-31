9e08f797bb3a8e18d95815207db92e5859923c66
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2019-05-31 22:23:36 +0200

    Tail all web logs

--- docker-web/web/init.sh
+++ docker-web/web/init.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 db=51.77.203.41
 
@@ -8,4 +8,11 @@ done
 
 cat /root/initdb.sql | mariadb -uroot -h"$db" -p"$DB_PASSWORD" || echo failed 
 
-lighttpd -D -f /etc/lighttpd/lighttpd.conf
+files=$(echo /var/log/lighttpd/{b2b_error.log,b2b_access.log,www_access.log,www_error.log,intranet_access.log,intranet_error.log})
+
+touch $files
+chmod 777 $files
+
+lighttpd -f /etc/lighttpd/lighttpd.conf
+
+tail -f $files

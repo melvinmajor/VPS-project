c5c7a9d8d0d068d36aabbeac50dccec47400d6cc
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2019-06-05 14:27:31 +0200

    Https redirect execpt for let's encrypt

--- docker-web/web/init.sh
+++ docker-web/web/init.sh
@@ -19,18 +19,4 @@ sleep 5
 
 certbot certonly --webroot -w /etc/lighttpd/certbot -d www.wt13.ephec-ti.be -d b2b.wt13.ephec-ti.be --email m.camposcasares@students.ephec.be --agree-tos --non-interactive 
 
-sleep 5
-
-echo restarting
-pkill lighttpd
-
-# will do for now
-sleep 2
-
-# uncomment include
-sed -e '/ssl.conf/s/^#//' /etc/lighttpd/lighttpd.conf -i
-
-# restart lighttpd
-lighttpd -f /etc/lighttpd/lighttpd.conf
-
 tail -f $files
